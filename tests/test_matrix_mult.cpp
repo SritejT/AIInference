2a10899361679a38d77a222d0519d1c861af3990
Sritej Tummuru <sritej.tummuru@gmail.com>
2026-03-29 12:34:20 +0100

    Added more matrix mult tests

--- tests/test_matrix_mult.cpp
+++ tests/test_matrix_mult.cpp
@@ -8,8 +8,8 @@ TEST(MatrixMultTest, SmallSquareMatrixMultTest) {
     FastTensor b = FastTensor({5.0f, 6.0f, 7.0f, 8.0f}, 2, 2);
 
     FastTensor result = a * b;
-    EXPECT_EQ(result.getWidth(), 2);
-    EXPECT_EQ(result.getHeight(), 2);
+    ASSERT_EQ(result.getWidth(), 2);
+    ASSERT_EQ(result.getHeight(), 2);
 
     float expected[4] = {19.0f, 22.0f, 43.0f, 50.0f};
 
@@ -26,8 +26,8 @@ TEST(MatrixMultTest, SmallRectMatrixMultTest) {
     FastTensor b = FastTensor({5.0f, 6.0f, 7.0f, 8.0f, 9.0f, 10.0f, 11.0f, 12.0f}, 2, 4);
 
     FastTensor result = a * b;
-    EXPECT_EQ(result.getWidth(), 4);
-    EXPECT_EQ(result.getHeight(), 2);
+    ASSERT_EQ(result.getWidth(), 4);
+    ASSERT_EQ(result.getHeight(), 2);
 
     float expected[8] = {23.0f, 26.0f, 29.0f, 32.0f, 51.0f, 58.0f, 65.0f, 72.0f};
 
@@ -37,4 +37,44 @@ TEST(MatrixMultTest, SmallRectMatrixMultTest) {
         j++;
     }
 
+}
+
+TEST(MatrixMultTest, LargeSquareMatrixMultTest) {
+    vector<float> a = vector<float>(1000000, 0.0f);
+    for (unsigned long i=0; i<1000000; i+=1001) {
+        a[i] = 1.0f;
+    }
+
+    vector<float> b = vector<float>(1000000, 0.0f);
+    for (unsigned long i=0; i<1000000; i++) {
+        b[i] = static_cast<float>(i);
+    }
+
+    FastTensor A = FastTensor(a, 1000, 1000);
+    FastTensor B = FastTensor(b, 1000, 1000);
+
+    FastTensor result = A * B;
+    ASSERT_EQ(result.getWidth(), 1000);
+    ASSERT_EQ(result.getHeight(), 1000);
+
+    int j=0;
+    for (auto r : result) {
+        EXPECT_EQ(r, static_cast<float>(j));
+        j++;
+    }
+}
+
+TEST(MatrixMultTest, LargeRectMatrixMultTest) {
+    vector<float> a = vector<float>(1000000, 1.0f);
+    vector<float> b = vector<float>(1000000, 1.0f);
+
+    FastTensor A = FastTensor(a, 1, 1000000);
+    FastTensor B = FastTensor(b, 1000000, 1);
+
+    FastTensor result = A * B;
+    ASSERT_EQ(result.getWidth(), 1);
+    ASSERT_EQ(result.getHeight(), 1);
+
+    EXPECT_EQ(*result.begin(), 1000000.0f);
+    
 }
