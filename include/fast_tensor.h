8b8737ef89ba464ab355eb225ebdcd4246b8d628
SritejT <sritej.tummuru@gmail.com>
2026-04-02 22:58:38 +0000

    Got rid of junk logic in FastTensor

--- include/fast_tensor.h
+++ include/fast_tensor.h
@@ -4,10 +4,7 @@
 using namespace std;
 
 class FastTensor : public Tensor {
-private:
-    void process_mult_rows(FastTensor* result, const FastTensor* other, size_t start_row, size_t end_row) const;
     
-    void process_add_rows(FastTensor* result, const FastTensor* other, size_t start_row, size_t end_row) const;
 public:
     FastTensor(size_t h, size_t w);
     FastTensor(vector<float> d, size_t h, size_t w);
--- src/tensors/fast_tensor.cpp
+++ src/tensors/fast_tensor.cpp
@@ -10,59 +10,6 @@ FastTensor::FastTensor(size_t h, size_t w): Tensor(h, w) {}
 
 FastTensor::FastTensor(vector<float> d, size_t h, size_t w): Tensor(d, h, w) {}
 
-void FastTensor::process_mult_rows(FastTensor* result, const FastTensor* other, size_t start_row, size_t end_row) const {
-
-    size_t new_width = other->getWidth();
-    for (size_t i = start_row; i < end_row; i++) {
-        size_t j=0;
-
-        for (; j+4 <= new_width; j+=4) {
-            
-            // Accumulates result[i][j:j+4]
-            float32x4_t acc = vdupq_n_f32(0.0f);
-
-            for (size_t k = 0; k < width; k++) {
-                float32x4_t va = vdupq_n_f32(data[i * width + k]);
-                float32x4_t vb = vld1q_f32(&other->data[k * new_width + j]);
-                acc = vmlaq_f32(acc, va, vb);
-            }
-
-            vst1q_f32(&result->data[i * new_width + j], acc);
-
-        }
-
-        // Do regular matrix mult for all j not covered by SIMD (i.e. if the j dimension is 
-        // not a multiple of 4)
-        for (; j < new_width; j++) {
-            float sum = 0.0f;
-            for (size_t k = 0; k < width; k++) {
-                sum += data[i * width + k] * other->data[k * new_width + j];
-            }
-            result->data[i * new_width + j] = sum;
-        }
-        
-    }
-}
-
-void FastTensor::process_add_rows(FastTensor* result, const FastTensor* other, size_t start_row, size_t end_row) const {
-
-    size_t i = start_row * width;
-
-    for (; i+4 < end_row * width; i+=4) {
-        float32x4_t va = vld1q_f32(&result->data[i]);
-        float32x4_t vb = vld1q_f32(&other->data[i]);
-        va = vaddq_f32(va, vb);
-        vst1q_f32(&result->data[i], va);
-    }
-
-    for (; i < end_row * width; i++) {
-        result->data[i] += other->data[i];
-    }
-
-}
-
-
-
 FastTensor FastTensor::operator*(const FastTensor& other) const {
     
     FastTensor result(height, other.getWidth());
