b35be5a58b802218e01f83368cb66d210ec6be41
SritejT <sritej.tummuru@gmail.com>
2026-04-09 22:24:25 +0000

    Added benchmarks for matrix transposition

---
+++ benchmarks/benchmark_matrix_transpose.cpp
@@ -0,0 +1,44 @@
+#include <benchmark/benchmark.h>
+#include "strategies/basic_tensor_strategy.h"
+#include "strategies/simd_tensor_strategy.h"
+#include "strategies/concurrent_row_tensor_strategy.h"
+#include "strategies/optimised_tensor_strategy.h"
+
+template <typename Strategy>
+static void TensorSquareMatTranspose(benchmark::State& state) {
+    int n = state.range(0);
+
+    auto strategy = std::make_shared<Strategy>();
+
+    Tensor a(std::vector<float>(n * n, 1.0f), n, n, strategy);
+    for (auto _ : state) {
+        Tensor c = a.transpose();
+        benchmark::DoNotOptimize(c);
+    }
+}
+
+template <typename Strategy>
+static void TensorVecTranspose(benchmark::State& state) {
+    int n = state.range(0);
+
+    auto strategy = std::make_shared<Strategy>();
+
+    Tensor a(std::vector<float>(n, 1.0f), n, 1, strategy);
+
+    for (auto _ : state) {
+        Tensor c = a.transpose();
+        benchmark::DoNotOptimize(c);
+    }
+}
+
+BENCHMARK(TensorSquareMatTranspose<BasicTensorStrategy>)->RangeMultiplier(2)->Range(2, 1024);
+BENCHMARK(TensorSquareMatTranspose<SimdTensorStrategy>)->RangeMultiplier(2)->Range(2, 1024);
+BENCHMARK(TensorSquareMatTranspose<ConcurrentRowTensorStrategy>)->RangeMultiplier(2)->Range(2, 1024);
+BENCHMARK(TensorSquareMatTranspose<OptimisedTensorStrategy>)->RangeMultiplier(2)->Range(2, 1024);
+
+BENCHMARK(TensorVecTranspose<BasicTensorStrategy>)->RangeMultiplier(2)->Range(2, 1024);
+BENCHMARK(TensorVecTranspose<SimdTensorStrategy>)->RangeMultiplier(2)->Range(2, 1024);
+BENCHMARK(TensorVecTranspose<ConcurrentRowTensorStrategy>)->RangeMultiplier(2)->Range(2, 1024);
+BENCHMARK(TensorVecTranspose<OptimisedTensorStrategy>)->RangeMultiplier(2)->Range(2, 1024);
+
+BENCHMARK_MAIN();
