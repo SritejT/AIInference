1e4b0699a9d7cf27e21b197f0130046e9b113409
SritejT <sritej.tummuru@gmail.com>
2026-04-10 19:28:36 +0000

    Added unit tests for scalar + unary ops

---
+++ tests/test_scalar_ops.cpp
@@ -0,0 +1,43 @@
+#include <gtest/gtest.h>
+#include "strategies/basic_tensor_strategy.h"
+#include "strategies/simd_tensor_strategy.h"
+#include "strategies/concurrent_row_tensor_strategy.h"
+#include "strategies/optimised_tensor_strategy.h"
+
+class TestScalarOps : public testing::TestWithParam<std::shared_ptr<TensorStrategy>> {};
+
+TEST_P(TestScalarOps, TestUnaryMinus) {
+    auto strategy = GetParam();
+    Tensor a = Tensor({1.0f, 2.0f, 3.0f, 4.0f}, 2, 2, strategy);
+    Tensor b = -a;
+    ASSERT_EQ(b.data[0], -1.0f);
+    ASSERT_EQ(b.data[1], -2.0f);
+    ASSERT_EQ(b.data[2], -3.0f);
+    ASSERT_EQ(b.data[3], -4.0f);
+}
+
+TEST_P(TestScalarOps, TestScalarMult) {
+    auto strategy = GetParam();
+    Tensor a = Tensor({1.0f, 2.0f, 3.0f, 4.0f}, 2, 2, strategy);
+    Tensor b = a * 2.0f;
+    ASSERT_EQ(b.data[0], 2.0f);
+    ASSERT_EQ(b.data[1], 4.0f);
+    ASSERT_EQ(b.data[2], 6.0f);
+    ASSERT_EQ(b.data[3], 8.0f);
+}
+
+TEST_P(TestScalarOps, TestScalarDiv) {
+    auto strategy = GetParam();
+    Tensor a = Tensor({1.0f, 2.0f, 3.0f, 4.0f}, 2, 2, strategy);
+    Tensor b = a / 2.0f;
+    ASSERT_EQ(b.data[0], 0.5f);
+    ASSERT_EQ(b.data[1], 1.0f);
+    ASSERT_EQ(b.data[2], 1.5f);
+    ASSERT_EQ(b.data[3], 2.0f);
+}
+
+INSTANTIATE_TEST_CASE_P(TensorStrategies, TestScalarOps, testing::Values(
+                std::make_shared<BasicTensorStrategy>(),
+                std::make_shared<SimdTensorStrategy>(),
+                std::make_shared<ConcurrentRowTensorStrategy>(),
+                std::make_shared<OptimisedTensorStrategy>()));
