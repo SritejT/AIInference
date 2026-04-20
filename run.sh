9724ed00a9ce4511bac407f30a45cb017601b2a9
SritejT <sritej.tummuru@gmail.com>
2026-04-20 21:17:07 +0000

    Remove MSan from CI for now

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 set -e
  
-cmake -S . -B build -DENABLE_TSAN=OFF -DENABLE_ASAN_UBSAN=ON -DENABLE_MSAN=OFF -DENABLE_BENCHMARKS=OFF
+cmake -S . -B build -DENABLE_TSAN=OFF -DENABLE_ASAN_UBSAN=OFF -DENABLE_BENCHMARKS=OFF
 cmake --build build --parallel
 build/tests/all_tests 
